846cae965c3f04b6313926610f297aa9ed4a2fde
Marcus Watson <mwatson@veracode.com>
2023-12-11 10:00:28 +0000

    Removed unnecessary Test Attachments

--- GraphGenerator.UnitTests/DependencyRetriever.cs
+++ GraphGenerator.UnitTests/DependencyRetriever.cs
@@ -8,9 +8,6 @@ namespace GraphGenerator.UnitTests
         {
             var sut = new DependencyRetriever();
 
-            TestContext.AddTestAttachment("artifacts\\CoreMain.dll", "Main Core Module");
-            TestContext.AddTestAttachment("artifacts\\CoreDependency.dll", "Dependency Core Module");
-
             var results = sut.GetDependencyByAssembly("artifacts\\CoreMain.dll", new List<string>(), false);
 
             var containsMain = results.Any(x => x.Name == "CoreMain");
@@ -27,9 +24,6 @@ namespace GraphGenerator.UnitTests
         public void Generate_Dependency_Graph_For_Framework()
         {
             var sut = new DependencyRetriever();
-
-            TestContext.AddTestAttachment("artifacts\\FrameworkMain.dll", "Main Framework Module");
-            TestContext.AddTestAttachment("artifacts\\FrameworkDependency.dll", "Dependency Framework Module");
 
             var results = sut.GetDependencyByAssembly("artifacts\\FrameworkMain.dll", new List<string>(), false);
 
